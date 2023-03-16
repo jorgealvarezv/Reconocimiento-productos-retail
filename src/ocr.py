019550f0c9500f40e07f2151680abc2a5107a337
Jorge <jorge.alvarezv@sansano.usm.cl>
2023-03-16 11:03:44 -0300

    ocr match funcional

--- src/ocr.py
+++ src/ocr.py
@@ -1,4 +1,10 @@
 import PaddleOCR.paddleocr as ocr
+from difflib import SequenceMatcher
+import pandas as pd
+import ast
+import time as t
+
+
 args = ocr.parse_args()
 
 def ocr_inference(args):
@@ -17,13 +23,87 @@ def ocr_inference(args):
         final.append(palabras)
     return final,tiempo,tiempo_init,imgs
 
+def ocr_match(list_words_bd,ocr_words,threshold):
+    
+    text = list_words_bd
+    text_list = ocr_words
+    n_words_bd = len(list_words_bd)
+    n_ocr_words = len(ocr_words)
+    score = 0
+    index_bd = 0
+
+    while index_bd < n_words_bd:
+        
+        index_ocr = 0
+        
+        while index_ocr < n_ocr_words:
+            palabra_bd = list_words_bd[index_bd]
+            largo_palabra_bd = len(palabra_bd)
+            palabra_ocr = ocr_words[index_ocr]
+            largo_palabra_ocr = len(palabra_ocr)
+            if largo_palabra_bd == largo_palabra_ocr:
+                if palabra_bd == palabra_ocr:
+                    if(index_bd == 0 or index_bd == 1):
+                        score += 4
+                        break
+                    elif(index_bd == 2 or index_bd == 3):
+                        score += 2
+                        break
+                    else:
+                        score += 1
+                        break
+            else:
+                
+                
+                matcher = SequenceMatcher(None,palabra_bd,palabra_ocr)
+                match = matcher.find_longest_match(0,
+                                        largo_palabra_bd,
+                                        0,
+                                        largo_palabra_ocr)
+                cadena_mas_larga = match.size
+                if cadena_mas_larga/largo_palabra_bd > threshold:
+                    
+                    if(index_bd == 0 or index_bd == 1):
+                        score += 4
+                        break
+                    elif(index_bd == 2 or index_bd == 3):
+                        score += 2
+                        break
+                    else:
+                        score += 1
+                        break
+            index_ocr += 1
+        index_bd += 1
+    
+    
+    return score            
+
 if __name__ == '__main__':
     results,tiempo,init,imgs=ocr_inference(args)
     
     print("\nTiempo de inicialización: ",init)
-
+    dataset='data/text_products.csv'
+    dataframe = pd.read_csv(dataset)
+    
+    palabras=['cardioaspirina', 'acido', 'acetilsalicilico', '100', 'mg', '20', 'comprimidos', 'recubrimiento', 'enterico', 'bayer']
+    p=['cardioaspirinai0oec', 'acidoacetilsalicilico100mg', '20', 'comprimidos', 'con', 'recubrimiento', 'enterico', 'antiagregante', 'plaquetario']
+    print(results[0])
+    score=ocr_match(palabras,results[0],0.8)
+    
     for result,time,img in zip(results,tiempo,imgs):
         print("----"*35)
         print(f"Se encontrarón {len(result)} palabras en la imagen {img} en un tiempo de {time} segundos")
         print(result)
+        print("----"*35)
+        start_match = t.time()
+        for (sku, text) in zip(dataframe['SKU'], dataframe['Text']):
+                sku = str(sku).zfill(6)
+                text = ast.literal_eval(text)
+                score=ocr_match(text,result,0.8)
+                if(score>8):
+                    print("El producto con SKU: ",sku," tiene un score de ",score)
+                    print("----"*35)
+        end=t.time()
+        print("Tiempo de match: ",end-start_match)
+
 
